a28d4b0d7486c682e1c89dcdfb612b910b691b47
mattfo <rinsesound@gmail.com>
2022-04-07 11:28:52 +0100

    added error handling

--- scheduled_jobs.py
+++ scheduled_jobs.py
@@ -34,8 +34,9 @@ def process_orders(app):
             json=payload
         )
 
+        app.logger.info("Response from endpoint: " + response.text)
         response.raise_for_status()
-
+        
         order.set_as_processed()
         save_order(order)
 
